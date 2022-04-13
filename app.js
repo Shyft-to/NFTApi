634f54b93b0825df64a2504362910fb4a62004fe
vgvishesh <vgvishesh2008@gmail.com>
2022-04-13 16:20:32 +0100

    upload images to ipfs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bp = require('body-parser')
 const nftHandler = require('./nftCreator');
 const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const ipfsAPI = require('ipfs-api');
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,8 @@ const port = 3000;
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 app.use(fileUpload());
+
+const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -31,19 +36,16 @@ app.post('/ownerOf', (req, res) => {
 
 app.post("/createNft", (req, res) => {
     console.log(req.body);
-    let file;
+    var uploadDetails;
     if (req.files) {
-        console.log("files attached with the request")
-        file = req.files.file;
-        file.mv('uploads/' + file.name, function (err) {
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            }
-        })
-        res.send('file uplaoded');
-
+        try {
+            uploadDetails = uploadDataToIpfs(req.files, createFileOnIpfs, createMetadataOnIpfs);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
     }
+
 
     // nftHandler.createNFT(req)
     //     .then((response) => res.send(response))
@@ -58,3 +60,41 @@ app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
 
+function uploadDataToIpfs(fileData, createFileOnIpfs, createMetadataOnIpfs) {
+    console.log("file details:");
+    console.log(fileData);
+
+    var uploadedFileName = uuidv4();
+    var uploadDir = "uploads";
+
+    file = fileData.file;
+    fileLocation = uploadDir + "/" + uploadedFileName;
+    file.mv(fileLocation, function (err) {
+        if (err) {
+            console.log(err);
+            throw new Error(err);
+        }
+
+        createFileOnIpfs(fileLocation, createMetadataOnIpfs)
+    })
+}
+
+// upload the data on ipfs
+function createFileOnIpfs(fileLocation, createMetadataOnIpfs) {
+    let diskFile = fs.readFileSync(fileLocation);
+    console.log("uploading file to ipfs");
+    let ipfsBuffer = new Buffer(diskFile);
+
+    ipfs.files.add(ipfsBuffer, function (err, file) {
+        if (err) {
+            console.log(err);
+            throw new Error(err)
+        }
+        console.log(file[0]);
+        createMetadataOnIpfs(file[0]);
+    });
+}
+
+function createMetadataOnIpfs(uploadedImageDetails) {
+
+}
