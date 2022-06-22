313a540dae493abba072116c59d3a0ca52c4489e
Pintu Das <rex@workwithme.in>
2022-06-22 18:26:16 +0530

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 echo "Deploying Server"
 
 # Restart Server
-pm2 stop all
+pm2 restart pm2.config.js
+
+# If previously not started, start fresh
 pm2 start pm2.config.js
 echo "S E R V E R   D E P L O Y E D"
